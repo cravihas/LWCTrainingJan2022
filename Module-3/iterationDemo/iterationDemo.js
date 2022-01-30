ef90877e1548278cc0be04a6c5ce44159de3842e
cravihas <98315101+cravihas@users.noreply.github.com>
2022-01-31 04:30:08 +0530

    Add files via upload

---
+++ Module-3/iterationDemo/iterationDemo.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class IterationDemo extends LightningElement {
+
+    accounts = [
+        {'id':'C1','name':'John Abraham','email':'ja@gmail.com'},
+        {'id':'C2','name':'Brad Pitt','email':'bpitt@gmail.com'},
+        {'id':'C3','name':'Angelina Jolie','email':'angelij@gmail.com'},
+        {'id':'C4','name':'Peter Parker','email':'spiderman@marvelcomics.com'},
+    ]
+}
